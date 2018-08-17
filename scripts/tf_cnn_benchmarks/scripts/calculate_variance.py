0c6693b6eda1b9abd2d17d86406045db020a1113
HinPeng <pieceseix@gmail.com>
2018-08-17 20:50:31 +0800

    update

--- scripts/tf_cnn_benchmarks/scripts/calculate_variance.py
+++ scripts/tf_cnn_benchmarks/scripts/calculate_variance.py
@@ -1,74 +1,82 @@
 import sys
 import os
 
-total_length = 100
-
-def calculate_variance(prev, curr):
-  assert(len(prev) == total_length)
-  assert(len(curr) == total_length)
-
-  max_variance = 0
-  for i in range(total_length):
-    v = abs(curr[i] - prev[i])
-    if v > max_variance:
-      max_variance = v
-
-  return max_variance
+class ProcessFile():
+  def __init__(self):
+    self.total_length = 100
+    self.counts = dict()
+    self.prev_value = dict()
+    self.all_layers = ['conv', 'pool', 'affine','dropout', 'batchnorm', 'lrn']
 
 
-def process_file(in_p, out_p=None):
-  prev_value = []
-  curr_value = []
+  def calculate_variance(self, prev, curr):
+    # assert(len(prev) == self.total_length)
+    # assert(len(curr) == self.total_length)
 
-  lines = in_p.readlines()
-  idx = 0
+    max_variance = 0
+    for i in range(self.total_length):
+      v = abs(curr[i] - prev[i])
+      if v > max_variance:
+        max_variance = v
 
-  start = 4
-  end = -8
-  for line in lines:
-    if not 'conv1' in line:
-      continue
-    else:
-      idx += 1
+    return max_variance
 
-    tmp = line.split('=')[1]
-    tmp = tmp[start:end]
-    tmp_value = tmp.split(' ')
 
-    if idx % 2 == 0:
-      curr_value = []
-    else:
-      prev_value = []
-    for v in tmp_value:
-      if idx % 2 == 0:
-        curr_value.append(float(v))
-      else:
-        prev_value.append(float(v))
-    if prev_value != [] and curr_value != []:
-      variance = calculate_variance(prev_value, curr_value)
-      if out_p:
-        out_p.write(str(variance) + '\n')
-      else:
-        print("The max variance between %d and %d is %f" % (idx - 1, idx, variance))
+  def process_file(self, in_p, out_dir=None):
+    lines = in_p.readlines()
 
-  in_p.close()
-  if out_p:
-    out_p.close()
+    for line in lines:
+      flag = False
+      for l in self.all_layers:
+        if l in line:
+          flag = True
+
+      if flag:
+        curr_value = []
+        layer_name = line.split('=')[0]
+        if self.counts.get(layer_name) != None:
+          self.counts[layer_name] += 1
+        else:
+          self.counts[layer_name] = 1
+
+        tmp = line.split('=')[1]
+        tmp = tmp.strip('[.]\n')
+        # tmp = tmp[start:end]
+        tmp_value = tmp.split(' ')
+
+        if self.counts[layer_name] == 1:
+          self.prev_value[layer_name] = []
+          for v in tmp_value:
+            self.prev_value[layer_name].append(float(v))
+        else:
+          for v in tmp_value:
+            curr_value.append(float(v))
+
+          variance = self.calculate_variance(self.prev_value[layer_name], curr_value)
+          self.prev_value[layer_name] = []
+          self.prev_value[layer_name].extend(curr_value)
+          if out_dir:
+            with open(out_dir+layer_name+'.txt', 'a') as out_p:
+              out_p.write(str(variance) + '\n')
+          else:
+            print("[%s] The max variance between %d and %d is %f" % (layer_name, self.counts[layer_name] - 1, self.counts[layer_name], variance))
+
+    in_p.close()
 
 def main():
+  pf = ProcessFile()
   if len(sys.argv) == 1:
     print("Error: need input file at least!")
 
   filename = sys.argv[1]
-  out_p = None
+  out_dir = None
 
   if len(sys.argv) == 3:
-    out_file = sys.argv[2]
-    out_p = open(out_file, 'a')
+    out_dir = sys.argv[2]
 
   in_p = open(filename)
-  process_file(in_p, out_p)
-  
+  pf.process_file(in_p, out_dir)
+
 
 if __name__ == '__main__':
   main()
--- scripts/tf_cnn_benchmarks/scripts/process_dir.py
+++ scripts/tf_cnn_benchmarks/scripts/process_dir.py
@@ -1,24 +1,26 @@
 import os
-from calculate_variance import process_file
+from calculate_variance import ProcessFile
 
 filepath="/vpublic01/frog/v-xuapen/benchmarks/scripts/tf_cnn_benchmarks/scripts/mem_log/"
-out_filepath="/vpublic01/frog/v-xuapen/benchmarks/scripts/tf_cnn_benchmarks/scripts/result/"
+out_filepath=filepath+'result/'
+os.mkdir(out_filepath)
 
 def process_dir(path):
   files = os.listdir(path)
   for fi in files:
     fi_d = os.path.join(path, fi)
     if os.path.isdir(fi_d):
-      process_dir(fi_d)
+      continue
+      # process_dir(fi_d)
     else:
       in_filename=str(fi)
       in_fullname=filepath + in_filename
-      out_filename=in_filename.split('.')[0] + ".txt"
-      out_fullname=out_filepath + out_filename
+      out_dir=out_filepath+in_filename.split('.')[0]+'/'
+      os.mkdir(out_dir)
 
       in_p=open(in_fullname)
-      out_p=open(out_fullname, 'a')
-      process_file(in_p, out_p=out_p)
+      pf = ProcessFile()
+      pf.process_file(in_p, out_dir=out_dir)
       # print(os.path.join(path, fi_d))
 
 if __name__ == '__main__':
